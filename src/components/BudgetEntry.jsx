1382837e1f80aa67e77d16aace01e448156dbfcf
PMurphyB <pmurphyguitar@gmail.com>
2026-02-07 09:15:27 -0700

    Moved Subfolders, Updating BudgetEntry.jsx

---
+++ src/components/BudgetEntry.jsx
@@ -0,0 +1,4 @@
+import { useEffect, useState } from "react";
+import { Card, Stack, HStack, VStack, Text, Input, Button, Field, Badge, Dialog, Portal, Textarea, useDisclosure } from "@chakra-ui/react";
+import { DollarSign, Save, X } from "lucide-react";
+
