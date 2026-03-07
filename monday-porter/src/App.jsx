e0e747fc91dbef6d85e28e758f2029fb1ada2269
Daniel Rawlins <daniel_rawlins@mines.edu>
2026-03-07 14:07:06 -0700

    Created the spending page

--- monday-porter/src/App.jsx
+++ monday-porter/src/App.jsx
@@ -3,6 +3,7 @@ import 'src/App.css'
 import Home from 'pages/Home.jsx'
 import SubsystemBudgets from 'pages/SubsystemBudgets.jsx'
 import OrderingQueue from 'pages/OrderingQueue.jsx'
+import Spending from 'pages/Spending.jsx'
 
 function App() {
   return (
@@ -10,13 +11,15 @@ function App() {
       <nav class="fixed">
         <Link to="/">Home</Link> | {" "}
         <Link to="/subsystemBudgets">Subsystem Budgets</Link> | {" "}
-        <Link to="/orderingQueue">Ordering Queue</Link>
+        <Link to="/orderingQueue">Ordering Queue</Link> | {" "}
+        <Link to="/spending">Spending</Link>
       </nav>
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/subsystemBudgets" element={<SubsystemBudgets />} />
         <Route path="/orderingQueue" element={<OrderingQueue/>} />
+        <Route path="/spending" element={<Spending/>} />
       </Routes>
     </>
   )
