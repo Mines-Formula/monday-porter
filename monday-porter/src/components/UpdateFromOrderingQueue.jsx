66ec7b10474256e5fb90b85b267d3868e5cf235a
Andrew Sautel <152945937+violinBhoy@users.noreply.github.com>
2026-04-04 16:56:37 -0600

    Added functionality to grab info from the ordering queue.
    Added functionality to grab info from the ordering queue.

---
+++ monday-porter/src/components/UpdateFromOrderingQueue.jsx
@@ -0,0 +1,10 @@
+function UpdateFromOrderingQueue() {
+    function update() {
+    }
+
+    return (
+        <button onClick={update}>Update pages from ordering queue</button>
+    )
+}
+
+export default UpdateFromOrderingQueue;
--- monday-porter/src/pages/Home.jsx
+++ monday-porter/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import { useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import UpdateFromOrderingQueue from '../components/UpdateFromOrderingQueue';
 
 function Home() {
     useEffect(() => {
         async function getInfo() {
             const res = await fetch('/api/orderingQueue');
+            console.log(res);
             const data = await res.json();
             console.log(data);
         }
@@ -16,6 +18,7 @@ function Home() {
             <h1 text-align="center">Home</h1>
             <main>
                 <Link to="/authorization" target="_blank">Authorize</Link>
+                <UpdateFromOrderingQueue/>
             </main>
         </>
     )
